8a4104173fb9c78a2fdafbbce8acb0c4923804e7
AloisKraus <Alois.Kraus@siemens-healthineers.com>
2022-03-25 22:15:47 +0100

    MakeRelase.cmd deletes previous publish folder and create zip file for release. Additionally the obj folder is deleted to ensure build consistency.

--- ETWAnalyzer/MakeRelease.cmd
+++ ETWAnalyzer/MakeRelease.cmd
@@ -3,14 +3,29 @@ echo Build Targets
 
 setlocal enabledelayedexpansion
 set ScriptLocation=%~d0%~p0
-set BinFolder48=!ScriptLocation!..\bin\Release\publishNet48
+set BinFolderNet48=!ScriptLocation!..\bin\Release\publishNet48
 set BinFolderNet6=!ScriptLocation!..\bin\Release\publishNet6
 
-dotnet publish /p:PublishProfile=!ScriptLocation!\ETWAnalyzer\Properties\PublishProfiles\Net60_SelfContained.pubxml /p:TargetFramework=net6.0-windows /p:PublishDir=!BinFolderNet6!
-dotnet publish /p:PublishProfile=!ScriptLocation!\ETWAnalyzer\Properties\PublishProfiles\Net48.pubxml /p:TargetFramework=net48 /p:PublishDir=!BinFolder48!
+set ObjFolder=!ScriptLocation!\obj
 
-call :DelFile "!BinFolder48!"
+set ReleaseZipNet6=!BinFolderNet6!\..\ETWAnalyzer_Net6.zip
+set ReleaseZipNet48=!BinFolderNet48!\..\ETWAnalyzer_Net48.zip
+
+del !ReleaseZipNet6! 2> NUL
+del !ReleaseZipNet48! 2> NUL
+
+rd /q /s !BinFolderNet48!
+rd /q /s !BinFolderNet6!
+rd /q /s !ObjFolder!
+
+dotnet publish /p:PublishProfile=!ScriptLocation!\ETWAnalyzer\Properties\PublishProfiles\Net60_SelfContained.pubxml /p:TargetFramework=net6.0-windows /p:PublishDir=!BinFolderNet6!
+dotnet publish /p:PublishProfile=!ScriptLocation!\ETWAnalyzer\Properties\PublishProfiles\Net48.pubxml /p:TargetFramework=net48 /p:PublishDir=!BinFolderNet48!
+
+call :DelFile "!BinFolderNet48!"
 call :DelFile "!BinFolderNet6!"
+
+7z a  !ReleaseZipNet6! -r "!BinFolderNet6!\*.*"
+7z a  !ReleaseZipNet48! -r "!BinFolderNet48!\*.*"
 
 goto :EOF
 
@@ -22,4 +37,5 @@ del ETWAnalyzer_Trace.log 2> NUL
 del "WPTx86 (DesktopEditions)-x86_en-us.msi"
 del "WPTx86 (OnecoreUAP)-x86_en-us.msi"
 rd /q /s x86
+rd /q /s arm64
 exit /B 1
