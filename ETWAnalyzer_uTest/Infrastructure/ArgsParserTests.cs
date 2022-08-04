095e8092840bd5a9f7e567ab12101463a43a27cc
Tejesh Pala <tejesh.pala@siemens-healthineers.com>
2022-08-04 16:46:02 +0200

    addition of argparser tests

---
+++ ETWAnalyzer_uTest/Infrastructure/ArgsParserTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ETWAnalyzer_uTest.Infrastructure
+{
+
+    class DummyParser : ArgParser
+    {
+        public DummyParser(string[] args) : base(args)
+        { }
+
+        public override string Help => throw new NotImplementedException();
+
+        public override void Parse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ArgsParserTests 
+    {
+        [Fact]
+        public void MyFirstTest()
+        {
+            Assert.True(ArgParser.IsNumberChar('1'));
+        }
+
+
+        [Fact]
+        public void DecimalSeparatorIsValid()
+        {
+            Assert.True(ArgParser.IsNumberChar('.'));
+        }
+
+        [Fact]
+        public void ThousandSeparatorIsValid()
+        {
+            Assert.True(ArgParser.IsNumberChar(','));
+        }
+
+
+        [Fact]
+        public void NegativeNumberWorks()
+        {
+            DummyParser parser = new DummyParser(new string[] { "-200" } );
+            Assert.False(parser.PeekNextArgumentSwitch(out string arg));
+        }
+
+        [Fact]
+        public void SingleMinusIsNotAnArgument()
+        {
+            DummyParser parser = new DummyParser(new string[] { "-" });
+            Assert.False(parser.PeekNextArgumentSwitch(out string arg));
+        }
+
+        [Fact]
+        public void MinusAndNotNumberIsArgument()
+        {
+            DummyParser parser = new DummyParser(new string[] { "-a" });
+            Assert.True(parser.PeekNextArgumentSwitch(out string arg));
+        }
+    }
+}
