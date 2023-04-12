cae6d1a05e587345cffd88cbd7876eac1dfc33cb
Tejesh Pala <tejesh.pala@siemens-healthineers.com>
2023-04-12 13:05:58 +0200

    added internal for MinMaxFilter for tests

--- ETWAnalyzer/EventDump/DumpFile.cs
+++ ETWAnalyzer/EventDump/DumpFile.cs
@@ -992,7 +992,7 @@ namespace ETWAnalyzer.EventDump
             SortOrders.Default,
         };
 
-        bool MinMaxFilter(MatchData data)
+        internal bool MinMaxFilter(MatchData data)
         {
             bool lret = true;
 
