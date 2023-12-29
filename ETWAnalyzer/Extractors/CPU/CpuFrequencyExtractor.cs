979ded7e0d70e110356d6921ea025de897748c52
Alois-xx <akraus1@gmx.de>
2023-12-30 00:46:26 +0100

    Add warning message when CPU Frequency data cannot be decoded.

--- ETWAnalyzer/Extractors/CPU/CpuFrequencyExtractor.cs
+++ ETWAnalyzer/Extractors/CPU/CpuFrequencyExtractor.cs
@@ -9,6 +9,8 @@ using ETWAnalyzer.Infrastructure;
 using ETWAnalyzer.Extract;
 using ETWAnalyzer.TraceProcessorHelpers;
 using ETWAnalyzer.Extract.CPU;
+using ETWAnalyzer.Analyzers.Exception.ResultPrinter;
+using ETWAnalyzer.ProcessTools;
 
 namespace ETWAnalyzer.Extractors.CPU
 {
@@ -35,6 +37,11 @@ namespace ETWAnalyzer.Extractors.CPU
 
                 foreach(Microsoft.Windows.EventTracing.Power.IProcessorFrequencyInterval cpu in myCpuFrequencies.Result.Intervals) 
                 {
+                    if( cpu.AverageFrequency == null)
+                    {
+                        Console.WriteLine($"Warning: File {results.SourceETLFileName} contains no CPU frequency ETW data, but CPU Frequency is null. This happens when the CaptureState for the Microsoft-Windows-Kernel-Processor-Power provider is missing.");
+                        break;
+                    }
                     frequencyData.AddFrequencyDuration((CPUNumber)cpu.Processor, (float) cpu.StartTime.RelativeTimestamp.TotalSeconds, (float) cpu.StopTime.RelativeTimestamp.TotalSeconds, (int) cpu.AverageFrequency.Value.TotalMegahertz);
                 }
 
