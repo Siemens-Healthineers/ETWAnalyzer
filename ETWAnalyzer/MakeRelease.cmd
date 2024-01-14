5e68cae50d7eb16a07ce072ff8be51d935751f7b
Alois-xx <akraus1@gmx.de>
2024-01-14 19:54:50 +0100

    - Update target framework from .NET 6.0 to .NET 8.0 - Updated dependent libraries

--- ETWAnalyzer/MakeRelease.cmd
+++ ETWAnalyzer/MakeRelease.cmd
@@ -4,28 +4,28 @@ echo Build Targets
 setlocal enabledelayedexpansion
 set ScriptLocation=%~d0%~p0
 set BinFolderNet48=!ScriptLocation!..\bin\Release\publishNet48
-set BinFolderNet6=!ScriptLocation!..\bin\Release\publishNet6
+set BinFolderNet8=!ScriptLocation!..\bin\Release\publishNet8
 
 set ObjFolder=!ScriptLocation!\obj
 
-set ReleaseZipNet6=!BinFolderNet6!\..\ETWAnalyzer_Net6.zip
+set ReleaseZipNet6=!BinFolderNet8!\..\ETWAnalyzer_Net8.zip
 set ReleaseZipNet48=!BinFolderNet48!\..\ETWAnalyzer_Net48.zip
 
 del !ReleaseZipNet6! 2> NUL
 del !ReleaseZipNet48! 2> NUL
 
 rd /q /s !BinFolderNet48!
-rd /q /s !BinFolderNet6!
+rd /q /s !BinFolderNet8!
 rd /q /s !ObjFolder!
 
-dotnet publish !ScriptLocation!\ETWAnalyzer.csproj /p:PublishProfile=!ScriptLocation!\ETWAnalyzer\Properties\PublishProfiles\Net60_SelfContained.pubxml /p:Configuration=Release /p:TargetFramework=net6.0-windows /p:PublishDir=!BinFolderNet6!
+dotnet publish !ScriptLocation!\ETWAnalyzer.csproj /p:PublishProfile=!ScriptLocation!\ETWAnalyzer\Properties\PublishProfiles\Net80_SelfContained.pubxml /p:Configuration=Release /p:TargetFramework=net8.0-windows /p:PublishDir=!BinFolderNet8!
 dotnet publish !ScriptLocation!\ETWAnalyzer.csproj /p:PublishProfile=!ScriptLocation!\ETWAnalyzer\Properties\PublishProfiles\Net48.pubxml /p:Configuration=Release /p:TargetFramework=net48 /p:PublishDir=!BinFolderNet48!
 dotnet pack !ScriptLocation!\ETWAnalyzer.csproj  /p:Configuration=Release 
 
 call :DelFile "!BinFolderNet48!"
-call :DelFile "!BinFolderNet6!"
+call :DelFile "!BinFolderNet8!"
 
-7z a  !ReleaseZipNet6! -r "!BinFolderNet6!\*.*"
+7z a  !ReleaseZipNet6! -r "!BinFolderNet8!\*.*"
 7z a  !ReleaseZipNet48! -r "!BinFolderNet48!\*.*"
 
 goto :EOF
