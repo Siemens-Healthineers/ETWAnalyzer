7d211eb1e63f0432c826736cc510c82e3dddf9b0
Alois Kraus <alois.kraus@siemens-healthineers.com>
2024-05-31 08:57:38 +0200

    add sample

--- ETWAnalyzer/MakeRelease.cmd
+++ ETWAnalyzer/MakeRelease.cmd
@@ -5,28 +5,35 @@ setlocal enabledelayedexpansion
 set ScriptLocation=%~d0%~p0
 set BinFolderNet48=!ScriptLocation!..\bin\Release\publishNet48
 set BinFolderNet8=!ScriptLocation!..\bin\Release\publishNet8
+set BinSamplesFolder=!ScriptLocation!..\Samples\bin\x64\Release
 
 set ObjFolder=!ScriptLocation!\obj
 
 set ReleaseZipNet6=!BinFolderNet8!\..\ETWAnalyzer_Net8.zip
 set ReleaseZipNet48=!BinFolderNet48!\..\ETWAnalyzer_Net48.zip
+set ReleaseSamples=!ScriptLocation!..\Samples_Binaries.zip
 
 del !ReleaseZipNet6! 2> NUL
 del !ReleaseZipNet48! 2> NUL
+del !ReleaseSamples! 2> NUL
 
 rd /q /s !BinFolderNet48!
 rd /q /s !BinFolderNet8!
+rd /q /s !BinSamplesFolder!
 rd /q /s !ObjFolder!
 
 dotnet publish !ScriptLocation!\ETWAnalyzer.csproj /p:PublishProfile=!ScriptLocation!\ETWAnalyzer\Properties\PublishProfiles\Net80_SelfContained.pubxml /p:Configuration=Release /p:TargetFramework=net8.0-windows /p:PublishDir=!BinFolderNet8!
 dotnet publish !ScriptLocation!\ETWAnalyzer.csproj /p:PublishProfile=!ScriptLocation!\ETWAnalyzer\Properties\PublishProfiles\Net48.pubxml /p:Configuration=Release /p:TargetFramework=net48 /p:PublishDir=!BinFolderNet48!
 dotnet pack !ScriptLocation!\ETWAnalyzer.csproj  /p:Configuration=Release 
 
+msbuild /p:Configuration=Release /p:Platform=x64 /p:OutDir=!BinSamplesFolder! !ScriptLocation!\..\Samples\EventLeak\EventLeak.vcxproj 
+
 call :DelFile "!BinFolderNet48!"
 call :DelFile "!BinFolderNet8!"
 
 7z a  !ReleaseZipNet6! -r "!BinFolderNet8!\*.*"
 7z a  !ReleaseZipNet48! -r "!BinFolderNet48!\*.*"
+7z a  !ReleaseSamples! -r "!BinSamplesFolder!\*.*"
 
 goto :EOF
 
