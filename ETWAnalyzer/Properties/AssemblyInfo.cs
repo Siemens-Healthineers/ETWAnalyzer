ab49e6b7ce2fef7b03e5c2f8395b68bbc49ba7e7
Alois Kraus <alois.kraus@siemens-healthineers.com>
2025-10-28 10:48:26 +0100

    Update version to 4.0.0.1

--- ETWAnalyzer/Properties/AssemblyInfo.cs
+++ ETWAnalyzer/Properties/AssemblyInfo.cs
@@ -47,4 +47,4 @@ using System.Runtime.Versioning;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.1")]
