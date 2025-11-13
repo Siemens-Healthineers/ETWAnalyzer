2aebc67d291cc3dd439a103dce7410489d5bc641
Alois Kraus <alois.kraus@siemens-healthineers.com>
2025-11-13 10:07:31 +0100

    - Fix: NullReferenceException when Ready metric is extracted but MachineDetailsExtractor did not set CPU instance. Now we initialize a default instance all the time.

--- ETWAnalyzer.Reader/Extract/ETWExtract.cs
+++ ETWAnalyzer.Reader/Extract/ETWExtract.cs
@@ -179,7 +179,7 @@ namespace ETWAnalyzer.Extract
         /// <summary>
         /// CPU Metrics
         /// </summary>
-        public CPUStats CPU { get; set; }
+        public CPUStats CPU { get; set; } = new CPUStats(null, null, null, null, null, null, null);
 
         /// <summary>
         /// CPU Metrics
