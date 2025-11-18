aa6ba45247eb5ac0cebc4e9c532c248ee81c0a4a
Alois Kraus <alois.kraus@siemens-healthineers.com>
2025-11-18 08:45:32 +0100

    Update make script to .NET 10.0

--- ETWAnalyzer/MakeRelease.cmd
+++ ETWAnalyzer/MakeRelease.cmd
@@ -3,30 +3,30 @@ echo Build Targets
 
 setlocal enabledelayedexpansion
 set ScriptLocation=%~d0%~p0
-set BinFolderNet8=!ScriptLocation!..\bin\Release\publishNet8
+set BinFolderNet10=!ScriptLocation!..\bin\Release\net10.0-windows\win-x64
 set BinSamplesFolder=!ScriptLocation!..\Samples\bin\x64\Release
 
 set ObjFolder=!ScriptLocation!\obj
 
-set ReleaseZipNet6=!BinFolderNet8!\..\ETWAnalyzer_Net8.zip
-set ReleaseSamples=!ScriptLocation!..\Samples_Binaries.zip
+set ReleaseZipNet10=!ScriptLocation!\..\bin\ETWAnalyzer_Net10.zip
+set ReleaseSamples=!ScriptLocation!..\bin\Samples_Binaries.zip
 
-del !ReleaseZipNet6! 2> NUL
+del !ReleaseZipNet10! 2> NUL
 del !ReleaseSamples! 2> NUL
 
-rd /q /s !BinFolderNet8!
+rd /q /s !BinFolderNet10!
 rd /q /s !BinSamplesFolder!
 rd /q /s !ObjFolder!
 
-dotnet publish !ScriptLocation!\ETWAnalyzer.csproj /p:PublishProfile=!ScriptLocation!\ETWAnalyzer\Properties\PublishProfiles\Net80_SelfContained.pubxml /p:Configuration=Release /p:TargetFramework=net8.0-windows /p:PublishDir=!BinFolderNet8!
+dotnet publish !ScriptLocation!\ETWAnalyzer.csproj /p:PublishProfile=!ScriptLocation!\ETWAnalyzer\Properties\PublishProfiles\Net10_SelfContained.pubxml /p:Configuration=Release /p:TargetFramework=net10.0-windows /p:PublishDir=!BinFolderNet10!
 dotnet build !ScriptLocation!\..\ETWAnalyzer.Reader\ETWAnalyzer.Reader.csproj  /p:Configuration=Release -f net48
 dotnet pack  !ScriptLocation!\..\ETWAnalyzer.Reader\ETWAnalyzer.Reader.csproj  /p:Configuration=Release 
 
 msbuild /p:Configuration=Release /p:Platform=x64 /p:OutDir=!BinSamplesFolder! !ScriptLocation!\..\Samples\EventLeak\EventLeak.vcxproj 
 
-call :DelFile "!BinFolderNet8!"
+call :DelFile "!BinFolderNet10!"
 
-7z a  !ReleaseZipNet6! -r "!BinFolderNet8!\*.*"
+7z a  !ReleaseZipNet10! -r "!BinFolderNet10!\*.*"
 7z a  !ReleaseSamples! -r "!BinSamplesFolder!\*.*"
 
 goto :EOF
